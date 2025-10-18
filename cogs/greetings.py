b65c691969ab3c725a1f98d2a8d8d5e04d3265cb
SomeoneButIdkWho <ayanarjya32@gmail.com>
2025-10-18 15:54:29 +0530

    Create greetings.py

---
+++ cogs/greetings.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+import random
+
+class Greetings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.greetings = ['Hello!', 'Hi there!', 'Hey!', 'Yo!']
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+        if any(word in message.content.lower() for word in ['hi', 'hello', 'hey']):
+            await message.channel.send(random.choice(self.greetings))
+
+def setup(bot):
+    bot.add_cog(Greetings(bot))
